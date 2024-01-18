199bfe27d08aa45e01aa9ec374aa22c03cf236cb
introtoxicXXl <minhajulabedin648@gmail.com>
2024-01-18 14:47:38 +0600

    user get and post api done

--- index.js
+++ index.js
@@ -9,9 +9,70 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/',(req,res)=>{
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q1v3e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const usersCollection = client.db('Stuff-boxes').collection('users');
+
+
+
+        // users get api 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+        // user post api 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existUser = await usersCollection.findOne(query);
+            if (existUser) {
+                return res.send({ message: 'User already in database' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Stuff Boxes is on the way with your Parcel')
 })
-app.listen(port,()=>{
-    console.log('stuff boxes right now on',port)
+app.listen(port, () => {
+    console.log('stuff boxes right now on', port)
 })
